7c1ec8887d22e4f4a8203b5833d5f151c7220162
KipkuruiRonald <ronaldkipkiruibii819@gmail.com>
2026-04-20 20:47:06 +0300

    Fix base path for dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/docverifier/',
+  base: mode === 'production' ? '/docverifier/' : '/',
   server: {
     host: "::",
     port: 8080,
